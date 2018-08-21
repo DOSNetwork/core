89eda8488599a17c38b2747accee4b5bad56db41
ericnien <nien0223@gmail.com>
2018-08-20 17:13:27 -0700

    [DOS-55]
    These code are from https://goethereumbook.org/.
    
    Test
    1)Generating Encrypted JSON key for Our Account
      geth account import path/to/private/key/file
    
      The above command will generate an encrypted JSON key file
      in ~/Library/Ethereum/keystore on a Mac and ~/.ethereum.
    
    2)Deploy a smart contrack or use existing smart contract in rinkeby
      go run contract_deploy.go
      existing contract address: 0xb26C47fAD204f7C95c9c7E14bA9fD63d744d48c7
    
    3)Subscribe events
      go run contract_subscribe.go
    
    4)Write something to smart contrack
      go run contract_write.go
    
    Signed-off-by: ericnien <nien0223@gmail.com>

---
+++ example/eth-event/contract_deploy.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/ethclient"
+
+	store "./contracts" // for demo
+)
+
+const key = ``
+
+func main() {
+	client, err := ethclient.Dial("https://rinkeby.infura.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth, err := bind.NewTransactor(strings.NewReader(key), "")
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	input := "1.0"
+	address, tx, instance, err := store.DeployStore(auth, client, input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(address.Hex())   // 0x147B8eb97fD247D06C4006D269c90C1908Fb5D54
+	fmt.Println(tx.Hash().Hex()) // 0xdae8ba5444eefdc99f4d45cd0c4f24056cba6a02cefbf78066ef9f4188ff7dc0
+
+	_ = instance
+}
