66e56d00ef91df1285bf6ba8aaa859311589b0fd
Xinrui <xiaorui89@hotmail.com>
2018-10-13 19:12:54 -0700

    Contract modification
    Remove the procedure of combining msg.sender with data during signature varifying.
    
    Add majority-vote function to ensure correctness of group public key.

--- blockchain/eth/contracts/DOSProxy.sol
+++ blockchain/eth/contracts/DOSProxy.sol
@@ -12,6 +12,7 @@ contract DOSProxy {
         address callbackAddr;
     }
 
+    uint groupSize;
     uint[] nodeId;
     // calling queryId => PendingQuery metadata
     mapping(uint => PendingQuery) pendingQueries;
@@ -19,6 +20,8 @@ contract DOSProxy {
     BN256.G2Point[] groupPubKeys;
     // groupIdentifier => isExisted
     mapping(bytes32 => bool) groups;
+    //publicKey => publicKey appearance
+    mapping(bytes32 => uint) pubKeyCounter;
     // Note: Make atomic changes to randomness metadata below.
     uint public lastUpdatedBlock;
     uint public lastRandomness;
@@ -45,13 +48,14 @@ contract DOSProxy {
     event LogValidationResult(
         uint8 trafficType,
         uint trafficId,
-        bytes message,
+        bytes data,
         uint[2] signature,
         uint[4] pubKey,
         bool pass
     );
     event LogInsufficientGroupNumber();
     event LogGrouping(uint[] NodeId);
+    event LogPublicKeyAccepted(uint x1, uint x2, uint y1, uint y2);
 
     function getCodeSize(address addr) internal constant returns (uint size) {
         assembly {
@@ -123,28 +127,20 @@ contract DOSProxy {
     internal
     returns (bool)
     {
-        // Validation
-        // TODO
-        // 1. Check msg.sender from registered and staked node operator.
-        // 2. Check msg.sender belongs to pendingQueries[queryId].handledGroup
-        // Clients actually signs (data || addr(selected_submitter)).
-        // TODO: Sync and change to sign ( sha256(data) || bytes32(address) )
-        bytes memory message = abi.encodePacked(data, bytes32(msg.sender));
-
         // Verification
         BN256.G1Point[] memory p1 = new BN256.G1Point[](2);
         BN256.G2Point[] memory p2 = new BN256.G2Point[](2);
         // The signature has already been applied neg() function offchainly to
         // fit requirement of pairingCheck function
         p1[0] = signature;
-        p1[1] = BN256.hashToG1(message);
+        p1[1] = BN256.hashToG1(data);
         p2[0] = BN256.P2();
         p2[1] = grpPubKey;
         if (!BN256.pairingCheck(p1, p2)) {
             emit LogValidationResult(
                 trafficType,
                 trafficId,
-                message,
+                data,
                 [signature.x, signature.y],
                 [grpPubKey.x[0], grpPubKey.x[1], grpPubKey.y[0], grpPubKey.y[1]],
                 false
@@ -154,7 +150,7 @@ contract DOSProxy {
             emit LogValidationResult(
                 trafficType,
                 trafficId,
-                message,
+                data,
                 [signature.x, signature.y],
                 [grpPubKey.x[0], grpPubKey.x[1], grpPubKey.y[0], grpPubKey.y[1]],
                 true
@@ -229,8 +225,13 @@ contract DOSProxy {
         bytes32 groupId = keccak256(abi.encodePacked(x1, x2, y1, y2));
         require(!groups[groupId], "group has already registered");
 
-        groupPubKeys.push(BN256.G2Point([x1, x2], [y1, y2]));
-        groups[groupId] = true;
+        pubKeyCounter[groupId] = pubKeyCounter[groupId] + 1;
+        if (pubKeyCounter[groupId] > groupSize / 2) {
+            groupPubKeys.push(BN256.G2Point([x1, x2], [y1, y2]));
+            groups[groupId] = true;
+            delete(pubKeyCounter[groupId]);
+            emit LogPublicKeyAccepted(x1, x2, y1, y2);
+        }
     }
 
     function getGroupPubKey(uint idx) public constant returns (uint[4]) {
@@ -247,6 +248,7 @@ contract DOSProxy {
     }
 
     function grouping(uint size) {
+        groupSize = size;
         uint[] memory toBeGrouped = new uint[](size);
         if (nodeId.length < size) {
             emit LogInsufficientGroupNumber();
@@ -315,8 +317,8 @@ library BN256 {
         }
     }
 
-    function hashToG1(bytes message) internal returns (G1Point) {
-        uint256 h = uint256(keccak256(message));
+    function hashToG1(bytes data) internal returns (G1Point) {
+        uint256 h = uint256(keccak256(data));
         return scalarMul(P1(), h);
     }
 
