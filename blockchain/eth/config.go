80448bf4caf5a5b19cf9c110fd6d46ff6ad771a6
Xinrui <xiaorui89@hotmail.com>
2018-10-15 14:53:19 -0700

    rinkeby connection protocol change

--- blockchain/eth/config.go
+++ blockchain/eth/config.go
@@ -9,7 +9,7 @@ type netConfig struct {
 }
 
 var (
-	rinkebyNode = initial("https://rinkeby.infura.io", "0xe1F4F37E193F4c993E754672768E688A477E2b2e")
+	rinkebyNode = initial("wss://rinkeby.infura.io/ws", "0xe1F4F37E193F4c993E754672768E688A477E2b2e")
 	privateNode = initial("ws://67.207.98.117:8546", "0xf59c7469b3668d0676DEDDb06B46E799ec2109ce")
 )
 
