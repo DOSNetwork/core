3887b2fe485e5a7c152a0171403f9c317bf5a2a9
Chen Hao Nien <chenhaonien@Chens-MacBook-Pro.local>
2018-11-21 17:21:22 -0800

    It is able to run 200 nodes on aws docker stack deploy -c peer-docker-compose.yml dhttest
    Signed-off-by: Chen Hao Nien <chenhaonien@Chens-MacBook-Pro.local>

--- p2p/peer_client.go
+++ p2p/peer_client.go
@@ -23,15 +23,19 @@ import (
 	"github.com/dedis/kyber"
 )
 
+const TIMEOUTFORHI = 60
+
 type PeerClient struct {
 	p2pnet       *P2P
 	conn         *net.Conn
 	rw           *bufio.ReadWriter
-	messageChan  chan P2PMessage
+	rxMessage    chan P2PMessage
+	txMessage    chan proto.Message
+	waitForHi    chan bool
+	done         chan bool
 	status       int
 	identity     dht.ID
 	pubKey       kyber.Point
-	wg           sync.WaitGroup
 	RequestNonce uint64
 	Requests     sync.Map
 	mux          sync.Mutex
@@ -43,99 +47,137 @@ type RequestState struct {
 	closeSignal chan struct{}
 }
 
-func (p *PeerClient) Dial(addr string) (err error) {
-	var conn net.Conn
-	fmt.Println(p.p2pnet.identity.Address, "Dial", addr)
-	conn, err = net.Dial("tcp", addr)
-	if err != nil {
-		fmt.Println("Dial err ", err)
+func NewPeerClient(p2pnet *P2P, conn *net.Conn, rxMessage chan P2PMessage) (peer *PeerClient, err error) {
+	peer = &PeerClient{
+		p2pnet:    p2pnet,
+		conn:      conn,
+		rxMessage: rxMessage,
+		txMessage: make(chan proto.Message, 100),
+		waitForHi: make(chan bool, 2),
+		done:      make(chan bool, 2),
+		rw:        bufio.NewReadWriter(bufio.NewReader(*conn), bufio.NewWriter(*conn)),
 	}
-	p.conn = &conn
+	go peer.receiveLoop()
+	err = peer.SayHi()
+	if err != nil {
+		close(peer.txMessage)
+		//close(peer.waitForHi)
+		//close(peer.done)
+		peer = nil
+	}
 	return
 }
 
-func (p *PeerClient) HandlePackages() {
+func (p *PeerClient) SendMessage(msg proto.Message) (err error) {
+	var prepared *internal.Package
+	prepared, err = p.PrepareMessage(msg)
+	if err != nil {
+		fmt.Println("PeerClient sendLoop err ", err)
+		return
+	}
+
+	prepared.RequestNonce = atomic.AddUint64(&p.RequestNonce, 1)
+	if err = p.SendPackage(prepared); err != nil {
+		fmt.Println("PeerClient sendLoop SendPackage ", err)
+	}
+	return
+}
+
+func (p *PeerClient) receiveLoop() {
+	//time.Sleep(TIMEOUTFORHI * time.Second)
+L:
 	for {
-		buf, err := p.receivePackage()
-		switch {
-		case err == io.EOF:
-			fmt.Println("PeerClient ", p.identity.Id, " end")
-			return
-		case err != nil:
-			fmt.Println("PeerClient ", p.identity.Id, " end")
-			return
-		}
+		select {
+		case <-p.done:
+			break L
+		default:
+			buf, err := p.receivePackage()
+			switch {
+			case err == io.EOF:
+				fmt.Println("PeerClient ", p.identity.Id, " EOF")
+				break L
+			case err != nil:
+				fmt.Println("PeerClient ", p.identity.Id, " err ", err)
+				break L
+			}
 
-		pa, ptr, err := p.decodePackage(buf)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if pa.GetRequestNonce() > 0 && pa.GetReplyFlag() {
-			if _state, exists := p.Requests.Load(pa.GetRequestNonce()); exists {
-				state := _state.(*RequestState)
-				select {
-				case state.data <- ptr.Message:
-				case <-state.closeSignal:
-				}
+			pa, ptr, err := p.decodePackage(buf)
+			if err != nil {
+				fmt.Println("PeerClient decodePackage err ", err)
 				continue
 			}
-		}
 
-		switch content := ptr.Message.(type) {
-		case *internal.Hi:
-			if len(p.identity.Id) == 0 {
-				p.identity.Id = content.GetId()
-				p.identity.Address = content.GetAddress()
-				p.identity.PublicKey = content.GetPublicKey()
-				pub := suite.G2().Point()
-				if err = pub.UnmarshalBinary(content.GetPublicKey()); err != nil {
+			if pa.GetRequestNonce() > 0 && pa.GetReplyFlag() {
+				if _state, exists := p.Requests.Load(pa.GetRequestNonce()); exists {
+					state := _state.(*RequestState)
+					select {
+					case state.data <- ptr.Message:
+					case <-state.closeSignal:
+					}
+					continue
+				}
+			}
+
+			switch content := ptr.Message.(type) {
+			case *internal.Hi:
+				if len(p.identity.Id) == 0 {
+					p.identity.Id = content.GetId()
+					p.identity.Address = content.GetAddress()
+					p.identity.PublicKey = content.GetPublicKey()
+					pub := suite.G2().Point()
+					if err = pub.UnmarshalBinary(content.GetPublicKey()); err != nil {
+						fmt.Println("PeerClient UnmarshalBinary err ", err)
+					}
+					p.pubKey = pub
+					p.p2pnet.peers.LoadOrStore(string(p.identity.Id), p)
+					p.p2pnet.routingTable.Update(p.identity)
+					p.waitForHi <- true
+				} else {
+					fmt.Println("Ignore Hi")
+				}
+			case *internal.Ping:
+				// Send pong to peer.
+				err := p.Reply(pa.GetRequestNonce(), &internal.Pong{})
+				if err != nil {
 					log.Fatal(err)
 				}
-				p.pubKey = pub
-				p.p2pnet.peers.LoadOrStore(string(p.identity.Id), p)
-				p.p2pnet.routingTable.Update(p.identity)
-				fmt.Println(p.p2pnet.identity.Address, "Receive Hi id = ", p.identity.Id, "from", p.identity.Address)
-				p.wg.Done()
-			} else {
-				fmt.Println("Ignore Hi")
-			}
-		case *internal.Ping:
-			// Send pong to peer.
-			err := p.Reply(pa.GetRequestNonce(), &internal.Pong{})
-			if err != nil {
-				log.Fatal(err)
-			}
-		case *internal.Pong:
-			peers := p.p2pnet.findNode(p.identity, dht.BucketSize, 8)
+			case *internal.Pong:
+				peers := p.p2pnet.findNode(p.identity, dht.BucketSize, 8)
 
-			// Update routing table w/ closest peers to self.
-			for _, peerID := range peers {
-				p.p2pnet.routingTable.Update(peerID)
+				// Update routing table w/ closest peers to self.
+				for _, peerID := range peers {
+					p.p2pnet.routingTable.Update(peerID)
+				}
+
+			case *internal.LookupNodeRequest:
+				// Prepare response.
+				response := &internal.LookupNodeResponse{}
+
+				// Respond back with closest peers to a provided target.
+				for _, peerID := range p.p2pnet.routingTable.FindClosestPeers(dht.ID(*content.GetTarget()), dht.BucketSize) {
+					id := internal.ID(peerID)
+					response.Peers = append(response.Peers, &id)
+				}
+
+				err := p.Reply(pa.GetRequestNonce(), response)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+			default:
+				msg := P2PMessage{Msg: *ptr, Sender: p.identity.Id}
+				p.rxMessage <- msg
 			}
-
-		case *internal.LookupNodeRequest:
-			// Prepare response.
-			response := &internal.LookupNodeResponse{}
-
-			// Respond back with closest peers to a provided target.
-			for _, peerID := range p.p2pnet.routingTable.FindClosestPeers(dht.ID(*content.GetTarget()), dht.BucketSize) {
-				id := internal.ID(peerID)
-				response.Peers = append(response.Peers, &id)
-			}
-
-			err := p.Reply(pa.GetRequestNonce(), response)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-		default:
-			msg := P2PMessage{Msg: *ptr, Sender: p.identity.Id}
-			p.messageChan <- msg
 		}
-		//fmt.Println("PeerClient ", p.id, " receive", string(buf))
-
 	}
+	_, ok := p.p2pnet.peers.Load(string(p.identity.Id))
+	if ok {
+		p.p2pnet.peers.Delete(string(p.identity.Id))
+	}
+	(*p.conn).Close()
+	close(p.done)
+	close(p.waitForHi)
+	fmt.Println("PeerClient receiveLoop done")
 }
 
 func (p *PeerClient) receivePackage() ([]byte, error) {
@@ -193,25 +235,32 @@ func (p *PeerClient) decodePackage(bytes []byte) (*internal.Package, *ptypes.Dyn
 	return pa, &ptr, nil
 }
 
-func (p *PeerClient) SayHi() {
-	//fmt.Println(p.p2pnet.identity.Address, "say hi")
+func (p *PeerClient) SayHi() (err error) {
 	pa := &internal.Hi{
 		PublicKey: p.p2pnet.identity.PublicKey,
 		Address:   p.p2pnet.identity.Address,
 		Id:        p.p2pnet.identity.Id,
 	}
 
-	prepared, err := p.PrepareMessage(pa)
-	if err != nil {
-		log.Fatal(err)
+	err = p.SendMessage(pa)
+
+	//Add a timer to avoid wait for Hi forever
+	timer := time.NewTimer(TIMEOUTFORHI * time.Second)
+L:
+	for {
+		select {
+		case <-timer.C:
+			p.done <- true
+			fmt.Println("Time expire")
+			err = errors.New("PeerClient: Time expire")
+			break L
+		case <-p.waitForHi:
+			_ = timer.Stop()
+			break L
+		}
 	}
 
-	prepared.RequestNonce = atomic.AddUint64(&p.RequestNonce, 1)
-	if err := p.SendPackage(prepared); err != nil {
-		log.Fatal(err)
-	}
-
-	p.wg.Wait()
+	return
 }
 
 func (p *PeerClient) SendPackage(msg proto.Message) error {
@@ -219,17 +268,23 @@ func (p *PeerClient) SendPackage(msg proto.Message) error {
 		return errors.New("network: message is null")
 	}
 	//Encode the package
-	bytes, _ := proto.Marshal(msg)
+	bytes, err := proto.Marshal(msg)
+	if err != nil {
+		fmt.Println("SendPackage Marshal err ", err)
+	}
 	// Serialize size.
 	prefix := make([]byte, 4)
 	binary.BigEndian.PutUint32(prefix, uint32(len(bytes)))
 	p.mux.Lock()
 	defer p.mux.Unlock()
 	bytes = append(prefix, bytes...)
+
 	if _, err := p.rw.Write(bytes); err != nil {
+		fmt.Println("SendPackage Write err ", err)
 		return err
 	}
 	if err := p.rw.Flush(); err != nil {
+		fmt.Println("!!!!SendPackage Flush err ", err)
 		return err
 	}
 
@@ -265,6 +320,7 @@ func (p *PeerClient) Request(req *Request) (proto.Message, error) {
 
 	prepared.RequestNonce = atomic.AddUint64(&p.RequestNonce, 1)
 	if err := p.SendPackage(prepared); err != nil {
+		fmt.Println("PeerClient Request err ", err)
 		return nil, err
 	}
 
@@ -301,6 +357,7 @@ func (p *PeerClient) Reply(nonce uint64, message proto.Message) error {
 	prepared.ReplyFlag = true
 
 	if err := p.SendPackage(prepared); err != nil {
+		fmt.Println("PeerClient Reply err ", err)
 		return err
 	}
 
