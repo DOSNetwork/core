0ac24bb23cbbf38bb6a66e56b9af6c9458f72025
Chen Hao Nien <chenhaonien@Chens-MacBook-Pro.local>
2018-11-29 17:57:50 -0800

    add peernode to Makefile [build] cd testing make docker-peernode [test] docker-compose -f testing/peerNode/findnode-docker-compose.yml up --scale peernode=20 docker-compose -f testing/peerNode/peer-docker-compose.yml  up --scale peernode=20
    Signed-off-by: Chen Hao Nien <chenhaonien@Chens-MacBook-Pro.local>

---
+++ p2p/peer_client_test.go
@@ -0,0 +1,16 @@
+package p2p
+
+import (
+	"fmt"
+	"math/big"
+	"testing"
+
+	"github.com/DOSNetwork/core/onchain"
+	"github.com/DOSNetwork/core/p2p"
+	"github.com/DOSNetwork/core/share/dkg/pedersen"
+	"github.com/DOSNetwork/core/suites"
+)
+
+func Test(test *testing.T) {
+
+}
