608544976e6757ff1595fd0463b2ed8c8a0a84cf
Chen Hao Nien <chenhaonien@Chens-MacBook-Pro.local>
2018-11-30 17:06:31 -0800

    Add a integration test for peerNode cd testing/peerNode go test -tags=integration
    Signed-off-by: Chen Hao Nien <chenhaonien@Chens-MacBook-Pro.local>

--- p2p/peer_client_test.go
+++ p2p/peer_client_test.go
@@ -1,16 +1,30 @@
 package p2p
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
-	"math/big"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
-
-	"github.com/DOSNetwork/core/onchain"
-	"github.com/DOSNetwork/core/p2p"
-	"github.com/DOSNetwork/core/share/dkg/pedersen"
-	"github.com/DOSNetwork/core/suites"
 )
 
-func Test(test *testing.T) {
+func NewPeerSendMessageTest(test *testing.T) {
+	cmdOutput := &bytes.Buffer{}
+	cmd := exec.Command("docker-compose", "-f", "peer-docker-compose.yml", "up", "--scale", "peernode=5")
+	cmd.Stdout = cmdOutput
+	cmd.Stderr = os.Stderr
+	cmd.Dir = filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "DOSNetwork", "core", "testing", "peerNode")
 
+	err := cmd.Run()
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+	}
+	f, err := os.Create(cmd.Dir + "/log")
+	defer f.Close()
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString(string(cmdOutput.Bytes()))
+	fmt.Printf("wrote %d bytes\n", n4)
+	w.Flush()
 }
