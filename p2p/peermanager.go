668070f2fbc8682b95747d5ad7ad9ef6edf1c6a8
fmlwlher <378050742@qq.com>
2018-12-12 17:31:34 +0800

    add peermanager
    (cherry picked from commit 97d6bb1035c4e4375214116adba6c54153d040a6)

--- p2p/peermanager.go
+++ p2p/peermanager.go
@@ -1,1 +1,73 @@
 package p2p
+
+import (
+	"sync"
+	"container/heap"
+)
+const MAXPEERCOUNT = 21
+
+type PeerManager struct {
+	mu sync.Mutex
+	peers sync.Map
+	parray PeerArray
+}
+
+func (pm *PeerManager) LoadOrStore(id string, peer *PeerConn) (actual *PeerConn, loaded bool){
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	ac, l := pm.peers.LoadOrStore(string(peer.identity.Id), peer)
+	loaded = l
+	if !l {
+		pm.mu.Lock()
+		heap.Push(&pm.parray, peer)
+		if len(pm.parray) > MAXPEERCOUNT {
+			p := heap.Pop(&pm.parray).(*PeerConn)
+			//p.End() //todo disconnet peer_conn
+			pm.peers.Delete(string(p.identity.Id))
+
+		}
+		actual = peer
+	}else {
+		actual = ac.(*PeerConn)
+	}
+	return
+}
+
+func (pm *PeerManager) Range(f func(key, value interface{}) bool) {
+	pm.peers.Range(f)
+}
+
+func (pm *PeerManager) GetPeerByID(id string) (value *PeerConn, ok bool) {
+	v, exist := pm.peers.Load(id)
+	ok = exist
+	if ok {
+		value = v.(*PeerConn)
+	}else {
+		value = nil
+	}
+	return
+}
+
+func (pm *PeerManager) DeletePeer(id string) {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	_, exist := pm.GetPeerByID(id)
+	if exist {
+		pm.peers.Delete(id)
+		index := -1
+		for i := 0; i<pm.parray.Len(); i++ {
+			if pm.parray[i].identity.Address == id {
+				index = i
+				break
+			}
+		}
+		if index != -1 {
+			heap.Remove(&pm.parray, index)
+		}
+	}
+
+}
+
+func (pm *PeerManager) PeerNum() int {
+	return len(pm.parray)
+}
--- p2p/peermanager_test.go
+++ p2p/peermanager_test.go
@@ -1,1 +1,36 @@
 package p2p
+
+import (
+	"testing"
+	"time"
+	"strconv"
+	"fmt"
+)
+
+func TestPeerManagerAddPeer(test *testing.T) {
+    pm := new(PeerManager)
+    for i:=0; i<50; i++ {
+    	pconn := new(PeerConn)
+    	pconn.identity.Address = strconv.Itoa(i)
+    	pconn.lastusedtime = time.Now()
+    	pm.LoadOrStore(strconv.Itoa(i), pconn)
+    	toppconn := pm.parray[0]
+    	if i < MAXPEERCOUNT {
+    		if toppconn.identity.Address != strconv.Itoa(0) {
+    			test.Fail()
+    			fmt.Println("Add Peer Test Failed")
+			}
+		}else {
+			if pm.parray.Len() > MAXPEERCOUNT {
+				test.Fail()
+				fmt.Println("Add Peer Test Failed")
+			}
+			if toppconn.identity.Address != strconv.Itoa(i-20) {
+				test.Fail()
+				fmt.Println("Add Peer Test Failed")
+			}
+		}
+		time.Sleep(1000)
+	}
+
+}
