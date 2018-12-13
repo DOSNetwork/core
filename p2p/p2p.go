a5502d21ed7b199f84c65aca612f28292eb021b6
Chen Hao Nien <chenhaonien@Chens-MacBook-Pro.local>
2018-12-12 16:45:15 -0800

    Use request and reply to implement SendMessage and update tstrategy for peer test
    Signed-off-by: Chen Hao Nien <chenhaonien@Chens-MacBook-Pro.local>

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
-	"math/big"
+	//"math/big"
 	"net"
 	"sort"
 	"strconv"
@@ -168,6 +168,7 @@ func (n *P2P) findPeer(id []byte, localOnly bool) (peer *PeerConn, found bool) {
 
 	// Find Peer from existing peerConn
 	if value, found = n.peers.GetPeerByID(string(id)); found {
+		//fmt.Println("!!!! Find Peer from existing peerConn ", id)
 		peer = value.(*PeerConn)
 
 		n.fromLocal++
@@ -186,6 +187,7 @@ func (n *P2P) findPeer(id []byte, localOnly bool) (peer *PeerConn, found bool) {
 	peers := n.routingTable.GetPeers()
 	if ip, ok := peers[string(id)]; ok {
 		if peer, err = n.connectTo(ip); err == nil {
+			fmt.Println("!!!! Find Peer from routing map ", id)
 			found = true
 
 			n.fromDht++
@@ -210,6 +212,7 @@ func (n *P2P) findPeer(id []byte, localOnly bool) (peer *PeerConn, found bool) {
 
 		// Find Peer from existing peerConn
 		if value, found = n.peers.GetPeerByID(string(id)); found {
+			fmt.Println("!!!! Find Peer from routing map ", id)
 			peer = value.(*PeerConn)
 			found = true
 
@@ -238,22 +241,23 @@ func (n *P2P) findPeer(id []byte, localOnly bool) (peer *PeerConn, found bool) {
 }
 
 func (n *P2P) SendMessage(id []byte, m proto.Message) (err error) {
-	var sendResult bool
 	var peer *PeerConn
 	var found bool
-	start := time.Now()
 	localOnly := false
 
 	if peer, found = n.findPeer(id, localOnly); found {
-		err = peer.SendMessage(m)
+		request := new(Request)
+		request.SetMessage(m)
+		request.SetTimeout(5 * time.Second)
+
+		_, err = peer.Request(request)
+		if err != nil {
+			fmt.Println("!!!!!!!!! SendMessage err.Error() ", err.Error())
+			//TODO :Need to check to see if this conn is a closed connection
+			return
+		}
 	}
 
-	n.log.WithFields(logrus.Fields{
-		"time-sendMessage": time.Since(start).Seconds(),
-		"nodeID":           new(big.Int).SetBytes(n.GetID()).String(),
-		"targetID":         new(big.Int).SetBytes(id).String(),
-		"send-result":      sendResult,
-	}).Info(err)
 	return
 }
 
@@ -427,16 +431,15 @@ func (lookup *lookupBucket) performLookup(n *P2P, targetID internal.ID, alpha in
 func (n *P2P) queryPeerByID(peerID internal.ID, targetID internal.ID, responses chan []*internal.ID) {
 	var client *PeerConn
 
-	targetPeer, loaded := n.peers.GetPeerByID(string(peerID.Id))
+	client, loaded := n.peers.GetPeerByID(string(peerID.Id))
 	if !loaded {
 		var err error
-		targetPeer, err = n.connectTo(peerID.Address)
+		client, err = n.connectTo(peerID.Address)
 		if err != nil {
 			responses <- []*internal.ID{}
 			return
 		}
 	}
-	client = targetPeer.(*PeerConn)
 
 	targetProtoID := internal.ID(targetID)
 
--- testing/peerNode/node/tstrategy_newpeer.go
+++ testing/peerNode/node/tstrategy_newpeer.go
@@ -13,53 +13,68 @@ import (
 type test1 struct{}
 
 func (r test1) StartTest(d *PeerNode) {
-	cmd := &internalMsg.Cmd{
-		Ctype: internalMsg.Cmd_SIGNIN,
-		Args:  []byte{},
-	}
-	pb := proto.Message(cmd)
-	for i := 0; i < len(d.nodeIPs); i++ {
-		ip := d.nodeIPs[i]
-		id, err := d.p.ConnectTo(ip)
-		if err != nil {
-			fmt.Println("NewPeer err", err)
-			d.log.WithField("event", "NewPeer err").Warn(ip)
+
+	if d.p.GetIP() == d.nodeIPs[0] {
+		fmt.Println("\nStartTest ", d.p.GetID(), " ", d.p.GetIP())
+		cmd := &internalMsg.Cmd{
+			Ctype: internalMsg.Cmd_SIGNIN,
+			Args:  []byte{},
 		}
-		d.checkroll[string(id)] = 0
-	}
-
-	for i := 0; i < d.numMessages; i++ {
-		for id, _ := range d.checkroll {
-
-			if err := d.p.SendMessage([]byte(id), pb); err != nil {
-				fmt.Println("SendMessage err ", err)
+		pb := proto.Message(cmd)
+		for i := 1; i < len(d.nodeIPs); i++ {
+			if d.p.GetIP() != d.nodeIPs[i] {
+				ip := d.nodeIPs[i]
+				id, err := d.p.ConnectTo(ip)
+				if err != nil {
+					fmt.Println("NewPeer err", err)
+				}
+				fmt.Println("ConnectTo To", id, " ", ip)
+				d.checkroll[string(id)] = 0
 			}
-			/*
-				tmp := make([]byte, len(id))
-				copy(tmp, id)
-				go func() {
-					if err := d.p.SendMessage(tmp, pb); err != nil {
-						fmt.Println("SendMessage err ", err)
+		}
+		for i := 0; i < d.numMessages; i++ {
+			for id, _ := range d.checkroll {
+				if err := d.p.SendMessage([]byte(id), pb); err != nil {
+					for err != nil {
+						err = d.p.SendMessage([]byte(id), pb)
 					}
-				}()*/
+				}
+			}
 		}
 	}
 }
 
 func (r test1) CheckResult(sender string, content *internalMsg.Cmd, d *PeerNode) {
-	if content.Ctype == internalMsg.Cmd_SIGNIN {
-		d.checkroll[sender] = d.checkroll[sender] + 1
-		if d.checkroll[sender] == d.numMessages {
-			delete(d.checkroll, sender)
-		}
+	if d.p.GetIP() == d.nodeIPs[0] {
+		if content.Ctype == internalMsg.Cmd_SIGNIN {
+			d.checkroll[sender] = d.checkroll[sender] + 1
+			if d.checkroll[sender] == d.numMessages {
+				delete(d.checkroll, sender)
+			}
 
-		d.log.WithField("event", "peer remain").Info(len(d.checkroll))
-
-		if len(d.checkroll) == 0 {
-			fmt.Println("test done")
-			d.log.WithField("event", "test done").Info()
-			d.MakeRequest(d.bootStrapIp, "post", d.nodeID)
+			if len(d.checkroll) == 0 {
+				fmt.Println("test done")
+				d.MakeRequest(d.bootStrapIp, "post", d.nodeID)
+			} else {
+				fmt.Println("wait for  = ", len(d.checkroll))
+				for id, _ := range d.checkroll {
+					fmt.Println("wait for ", []byte(id))
+				}
+				fmt.Println("==================== ")
+			}
 		}
+	} else {
+		cmd := &internalMsg.Cmd{
+			Ctype: internalMsg.Cmd_SIGNIN,
+			Args:  []byte{},
+		}
+		pb := proto.Message(cmd)
+		if err := d.p.SendMessage([]byte(sender), pb); err != nil {
+			for err != nil {
+				err = d.p.SendMessage([]byte(sender), pb)
+			}
+		}
+		d.MakeRequest(d.bootStrapIp, "post", d.nodeID)
 	}
 }
 
