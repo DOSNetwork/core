f79e29acd54379114a3d47c5fd26f547b0dc3fa0
ericnien <nien0223@gmail.com>
2019-01-07 17:17:14 -0800

    Add a log interface.
    Signed-off-by: ericnien <nien0223@gmail.com>

---
+++ log/root.go
@@ -0,0 +1,48 @@
+package log
+
+import (
+	"github.com/bshuster-repo/logrus-logstash-hook"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	root = &logger{}
+)
+
+func New(key string, value interface{}) Logger {
+	return root.New(key, value)
+}
+
+func init() {
+	//IP,Subject and appName should read from environment variables
+	hook, err := logrustash.NewHook("tcp", "163.172.36.173:9500", "peer")
+	if err != nil {
+		logrus.Error(err)
+	}
+	logrus.AddHook(hook)
+	root.entry = logrus.WithFields(logrus.Fields{
+		"subject": "subject",
+		"appName": "appName",
+	})
+}
+
+// Info is a convenient alias for Root().Info
+func Info(msg string) {
+	root.Info(msg)
+}
+func Warn(msg string) {
+	root.Warn(msg)
+}
+
+func Error(err error) {
+	root.Error(err)
+}
+
+func Fatal(msg string) {
+	root.Fatal(msg)
+}
+
+// Info is a convenient alias for Root().Info
+func Metrics(key string, value interface{}) {
+	root.Metrics(key, value)
+}
