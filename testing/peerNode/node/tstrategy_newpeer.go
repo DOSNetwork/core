5bfde9ee62bfaa20830adb70ec67d8992f7491bf
ericnien <nien0223@gmail.com>
2019-03-24 01:36:17 -0700

    Change test to simulate dkg case Each node sends multiple messages to other nodes at the same time
    Signed-off-by: ericnien <nien0223@gmail.com>

--- testing/peerNode/node/tstrategy_newpeer.go
+++ testing/peerNode/node/tstrategy_newpeer.go
@@ -11,6 +11,8 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/DOSNetwork/core/share/dkg/pedersen"
@@ -20,39 +22,44 @@ import (
 	"github.com/DOSNetwork/core/suites"
 	"github.com/DOSNetwork/core/testing/peerNode/internalMsg"
 
-	log "github.com/DOSNetwork/core/log"
+	//log "github.com/DOSNetwork/core/log"
 	"github.com/golang/protobuf/proto"
 )
 
 type test1 struct{}
 
 func (r test1) StartTest(d *PeerNode) {
-	fmt.Println("StartTest")
 	cmd := &internalMsg.Cmd{
 		Ctype: internalMsg.Cmd_SIGNIN,
 		Args:  []byte{},
 	}
 	pb := proto.Message(cmd)
+	var wg sync.WaitGroup
+	wg.Add(len(d.nodeIDs) - 1)
 	for i := 0; i < len(d.nodeIPs); i++ {
-		if d.p.GetIP() != d.nodeIPs[i] {
-			ip := d.nodeIPs[i]
-			id, err := d.p.ConnectTo(ip)
-			if err != nil {
-				log.Fatal(err)
-			}
-			d.checkroll[string(id)] = 0
-			d.idSet[string(id)] = struct{}{}
-		}
-	}
-	for i := 0; i < d.numMessages; i++ {
-		for i := 0; i < len(d.nodeIDs); i++ {
-			if !bytes.Equal(d.p.GetID(), d.nodeIDs[i]) {
-				if _, err := d.p.Request(d.nodeIDs[i], pb); err != nil {
-					log.Fatal(err)
+		if !strings.Contains(d.p.GetIP(), d.nodeIPs[i]) {
+			go func(j int) {
+				defer wg.Done()
+				ip := d.nodeIPs[j]
+				id, err := d.p.ConnectTo(ip)
+				if err != nil {
+					os.Exit(1)
 				}
-			}
+				for k := 0; k < d.numMessages; k++ {
+					start := time.Now()
+					if _, err := d.p.Request(id, pb); err != nil {
+						os.Exit(1)
+					} else {
+						fmt.Println("Request done", time.Since(start).Nanoseconds()/1000)
+					}
+
+				}
+			}(i)
 		}
 	}
+	wg.Wait()
+
+	d.FinishTest()
 }
 
 func (r test1) CheckResult(sender string, content *internalMsg.Cmd, d *PeerNode) {
@@ -67,16 +74,64 @@ func (r test2) StartTest(d *PeerNode) {
 		Args:  []byte{},
 	}
 	pb := proto.Message(cmd)
-	for i := 0; i < len(d.nodeIDs); i++ {
-		if !bytes.Equal(d.p.GetID(), d.nodeIDs[i]) {
-			fmt.Println("SendMessage from ", d.p.GetID(), " to ", d.nodeIDs[i])
-			if err := d.p.SendMessage(d.nodeIDs[i], pb); err != nil {
-				fmt.Println("SendMessage err ", err)
-				for {
+	/*
+		for i := 0; i < len(d.nodeIDs); i++ {
+			if !bytes.Equal(d.p.GetID(), d.nodeIDs[i]) {
+				start := time.Now()
+				if _, err := d.p.Request(d.nodeIDs[i], pb); err != nil {
+					fmt.Println(start, " ->", time.Now(), "testfailed ", d.p.GetID(), " ->", d.nodeIDs[i], err)
+					os.Exit(1)
+				} else {
+					fmt.Println("Test done", time.Since(start).Nanoseconds()/1000)
 				}
 			}
 		}
+		time.Sleep(10 * time.Second)
+	*/
+	var wg sync.WaitGroup
+	/*
+		wg.Add((len(d.nodeIDs) - 1) * d.numMessages)
+		fmt.Println("Test start test2", (len(d.nodeIDs)-1)*d.numMessages)
+
+		for i := 0; i < len(d.nodeIDs); i++ {
+			if !bytes.Equal(d.p.GetID(), d.nodeIDs[i]) {
+				for j := 0; j < d.numMessages; j++ {
+					fmt.Println("Test start count", i, j)
+					go func(p p2p.P2PInterface, id []byte) {
+						fmt.Println("Test start goroutine")
+						start := time.Now()
+						defer wg.Done()
+						if _, err := p.Request(id, pb); err != nil {
+							fmt.Println(start, " ->", time.Now(), "testfailed ", p.GetID(), " ->", id, err)
+							os.Exit(1)
+						} else {
+							fmt.Println("Test done", time.Since(start).Nanoseconds()/1000)
+						}
+					}(d.p, d.nodeIDs[i])
+				}
+			}
+		}
+	*/
+
+	wg.Add((len(d.nodeIDs) - 1))
+	for i := 0; i < len(d.nodeIDs); i++ {
+		if !bytes.Equal(d.p.GetID(), d.nodeIDs[i]) {
+			go func(i int) {
+				defer wg.Done()
+				start := time.Now()
+				for j := 0; j < d.numMessages; j++ {
+					if _, err := d.p.Request(d.nodeIDs[i], pb); err != nil {
+						os.Exit(1)
+					} else {
+						fmt.Println("Test done", time.Since(start).Nanoseconds()/1000)
+					}
+				}
+			}(i)
+		}
 	}
+
+	wg.Wait()
+
 	d.FinishTest()
 }
 
@@ -93,17 +148,11 @@ func (r test3) StartTest(d *PeerNode) {
 		//d.log.Fatal(err)
 	}
 
-	suite := suites.MustFind("bn256")
-	p2pDkg, err := dkg.CreateP2PDkg(d.p, suite)
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	for idx, id := range d.nodeIDs {
 		if bytes.Compare(d.p.GetID(), id) == 0 {
 			start := idx / groupSize * groupSize
 			group := d.nodeIDs[start : start+groupSize]
-			dkgEvent, errChan := p2pDkg.Start(context.Background(), group, fmt.Sprintf("%x", group))
+			dkgEvent, errChan := d.p2pDkg.Start(context.Background(), group, fmt.Sprintf("%x", group))
 			go func() {
 				for err := range errChan {
 					fmt.Println("errorChan", err)
