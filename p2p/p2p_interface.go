42d7d729d98f904f97c59044b310d242596f9bb7
ericnien <nien0223@gmail.com>
2019-03-24 01:44:11 -0700

    Refactor p2p interface
    Signed-off-by: ericnien <nien0223@gmail.com>

--- p2p/p2p_interface.go
+++ p2p/p2p_interface.go
@@ -1,49 +1,45 @@
 package p2p
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net"
-	"sync"
 
 	"github.com/DOSNetwork/core/log"
 	"github.com/DOSNetwork/core/suites"
-	"github.com/dedis/kyber"
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 )
 
-var (
-	suite = suites.MustFind("bn256")
-)
-
-func genPair() (kyber.Scalar, kyber.Point) {
-	secret := suite.Scalar().Pick(suite.RandomStream())
-	public := suite.Point().Mul(secret, nil)
-	return secret, public
-}
+var logger log.Logger
 
 func CreateP2PNetwork(id []byte, port string) (P2PInterface, error) {
-	p := &P2P{
-		replyPeers:   new(sync.Map),
-		requestPeers: new(sync.Map),
-		suite:        suite,
-		messages:     make(chan P2PMessage, 100),
-		port:         port,
-		logger:       log.New("module", "p2p"),
+	suite := suites.MustFind("bn256")
+	logger = log.New("module", "p2p")
+	p := &Server{
+		suite:     suite,
+		messages:  make(chan P2PMessage, 100),
+		subscribe: make(chan Subscription),
+		unscribe:  make(chan Subscription),
+		port:      port,
 	}
-	p.identity.Id = id
+	p.ctx, p.cancel = context.WithCancel(context.Background())
+	p.secKey = suite.Scalar().Pick(suite.RandomStream())
+	p.pubKey = suite.Point().Mul(p.secKey, nil)
+	p.id = id
 	ip, err := GetLocalIP()
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
 	}
+	p.address = ip
+
 	p.cluster, err = SetupCluster(ip, id)
 	if err != nil {
-		fmt.Println(err)
+		logger.Error(err)
 		return nil, err
 	}
-	fmt.Println("Done SetupCluster")
 	return p, nil
 }
 
@@ -68,7 +64,6 @@ type P2PMessage struct {
 	Msg          ptypes.DynamicAny
 	Sender       []byte
 	RequestNonce uint64
-	PeerConn     *PeerConn
 }
 
 type P2PInterface interface {
@@ -78,11 +73,9 @@ type P2PInterface interface {
 	Join(bootstrapIp string) error
 	ConnectTo(IpAddr string) (id []byte, err error)
 	Leave()
-	SendMessage(id []byte, msg proto.Message) error
 	Members() int
-	Request(id []byte, m proto.Message) (msg proto.Message, err error)
+	Request(id []byte, m proto.Message) (msg P2PMessage, err error)
 	Reply(id []byte, nonce uint64, m proto.Message) (err error)
 	SubscribeEvent(chanBuffer int, messages ...interface{}) (outch chan P2PMessage, err error)
 	UnSubscribeEvent(messages ...interface{})
-	CloseMessagesChannel()
 }
