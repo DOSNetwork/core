300e8e55cb14232d2362785c22c8cbe56b6dda76
jonnyhsy <hsyjonny@gmail.com>
2019-05-23 04:00:46 -0700

    update settings and deployment script

--- vps.sh
+++ vps.sh
@@ -1,157 +1,156 @@
-#/bin/bash
+#!/bin/bash
 
 setting="./dos.setting"
 # If file exists
-if [[ -f "$setting" ]]
-then
-    source $setting
-	if [ -z "$DOSIMAGE" ];
-	then
-		echo "Please assign a value to DOSIMAGE in dos.setting"
-		exit
-	fi
-	if [ -z "$USER" ];
-	then
-		echo "Please assign a value to USER in dos.setting"
-		exit
-	fi
-	if [ -z "$VPSIP" ];
-	then
-		echo "Please assign a value to VPSIP in dos.setting"
-		exit
-	fi
-	if [ -z "$VPSKEY" ];
-	then
-		echo "Please assign a value to VPSKEY in dos.setting"
-		exit
-	fi
-	if [ -z "$KEYSTORE" ];
-	then
-		echo "Please assign a value to KEYSTORE in dos.setting"
-		exit
-	fi
-	if [ -z "$GETHPOOL" ];
-	then
-		echo "Please assign a value to GETHPOOL in dos.setting"
-		exit
-	fi
+if [[ -f "$setting" ]]; then
+  source $setting
+  if [ -z "$DOSIMAGE" ]; then
+    echo "Please assign a value to DOSIMAGE field in dos.setting"
+    exit
+  fi
+  if [ -z "$USER" ]; then
+    echo "Please assign a value to USER field in dos.setting"
+    exit
+  fi
+  if [ -z "$IP" ]; then
+    echo "Please assign a value to IP field in dos.setting"
+    exit
+  fi
+  if [ -z "$SSHKEY" ]; then
+    echo "Please assign a value to SSHKEY field in dos.setting"
+    exit
+  fi
+  if [ -z "$KEYSTORE" ]; then
+    echo "Please assign a value to KEYSTORE in dos.setting"
+    exit
+  fi
+  if [ -z "$GETHPOOL" ]; then
+    echo "Please assign a value to GETHPOOL in dos.setting"
+    exit
+  fi
 else
-    echo "Can't find dos.setting"
-	exit
+  echo "Can't find dos.setting"
+  exit
 fi
-GETHPOOL=$GETHPOOL";ws://"$VPSIP":8546"
-DIR="/home/"$USER
+
+GETHPOOL="$GETHPOOL;ws://$IP:8546"
+if [ "$USER" == "root" ]; then
+  DIR="/root"
+else
+  DIR="/home/$USER"
+fi
 
 install_lightnode(){
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'ps cax | grep geth > /dev/null'
-  if [ $? -eq 0 ]
-  then
-	echo "Geth Process is running."
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'ps cax | grep geth > /dev/null'
+  if [ $? -eq 0 ]; then
+    echo "Geth Process is running."
   else
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo apt-get update'
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo add-apt-repository -y ppa:ethereum/ethereum'
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo apt-get update'
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo apt-get install ethereum'
-	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $VPSKEY rinkeby.json $USER@$VPSIP:~/
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'geth --datadir='$DIR'/.rinkeby init rinkeby.json'
-	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $VPSKEY static-nodes.json $USER@$VPSIP:$DIR/.rinkeby/geth/static-nodes.json
-	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $VPSKEY geth.service  $USER@$VPSIP:~/
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo chmod 322 geth.service'
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo mv geth.service /lib/systemd/system/geth.service'
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo systemctl enable geth'
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo systemctl start geth'
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo apt-get update'
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo add-apt-repository -y ppa:ethereum/ethereum'
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo apt-get update'
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo apt-get install ethereum'
+    wget https://www.rinkeby.io/rinkeby.json -q -O rinkeby.json
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSHKEY rinkeby.json $USER@$IP:~/
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'geth --datadir='$DIR'/.rinkeby init rinkeby.json'
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSHKEY static-nodes.json $USER@$IP:$DIR/.rinkeby/geth/static-nodes.json
+    rm -f rinkeby.json
+    sed -i 's|xxx|'$DIR'|g' geth.service
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSHKEY geth.service  $USER@$IP:~/
+    git checkout -- geth.service
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo chmod 322 geth.service'
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo mv geth.service /lib/systemd/system/geth.service'
+#    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo systemctl enable geth'
+#    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo systemctl start geth'
   fi
 }
 
 start_lightnode(){
   echo "start_lightnode"
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'ps cax | grep geth > /dev/null'
-  if [ $? -eq 0 ]
-  then
-	echo "Geth Process is running."
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'ps cax | grep geth > /dev/null'
+  if [ $? -eq 0 ]; then
+    echo "Geth Process is running."
   else
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo systemctl start geth'
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo systemctl start geth'
   fi
 }
 
 install_client(){
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | [ -f "client" ] && rm client'
-	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $VPSKEY client  $USER@$VPSIP:$DIR/
-	update_config
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | [ -f "client" ] && rm client'
+  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSHKEY client  $USER@$IP:$DIR/
+  update_config
 }
 
 update_config(){
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'mkdir -p '$DIR'/dos'
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'mkdir -p '$DIR'/credential'
-	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $VPSKEY config.json $USER@$VPSIP:~/
-	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $VPSKEY $KEYSTORE $USER@$VPSIP:$DIR'/credential/'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'mkdir -p '$DIR'/dos'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'mkdir -p '$DIR'/credential'
+  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSHKEY config.json $USER@$IP:~/
+  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSHKEY $KEYSTORE $USER@$IP:$DIR'/credential/'
 }
 
 run_client(){
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'export GETHPOOL="'$GETHPOOL'";export PUBLICIP='$VPSIP'; echo -n Password:;read -s password ;export PASSPHRASE=$password ;setsid ./client start >dos/doslog 2>&1'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'export GETHPOOL="'$GETHPOOL'";export PUBLICIP='$IP'; echo -n Password:;read -s password ;export PASSPHRASE=$password ;setsid ./client start >dos/doslog 2>&1'
 }
 
 stop_client(){
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP './client stop'
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP '[ -f "client.pid" ] && rm client.pid'
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'sudo pkill -f client'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP './client stop'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP '[ -f "client.pid" ] && rm client.pid'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'sudo pkill -f client'
 }
 
 show_proxy(){
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP './client cmd showProxyStatus'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP './client cmd showProxyStatus'
 }
 
 clientInfo(){
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP './client cmd showStatus'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP './client cmd showStatus'
 }
 
 trigger_guardian(){
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP './client cmd triggerGuardian'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP './client cmd triggerGuardian'
 }
 
 check_client(){
-    result=$(ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'pgrep -x client')
-    newlog="doslog_$VPSIP"
-    if [ -z "$result" ];
-    then scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $VPSKEY $USER@$VPSIP:~/dos/doslog $newlog;
-    else echo "client is running";
-    fi
+  result=$(ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'pgrep -x client')
+  newlog="doslog_$IP"
+  if [ -z "$result" ]; then
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSHKEY $USER@$IP:~/dos/doslog $newlog;
+  else
+    echo "client is running";
+  fi
 }
 
 case "$1" in
   "install")
-	install_lightnode
-	install_client
-	;;
+    install_lightnode
+    install_client
+    ;;
   "run")
-	run_client
-	;;
+    run_client
+    ;;
   "stop")
-	stop_client
-	;;
+    stop_client
+    ;;
   "check")
-	check_client
+    check_client
     ;;
   "proxyInfo")
-	show_proxy
-	;;
+    show_proxy
+    ;;
   "clientInfo")
-	clientInfo
-	;;
+    clientInfo
+    ;;
   "guardian")
-	guardian
-	;;
+    guardian
+    ;;
   *)
     echo "Usage: bash vps.sh [OPTION]"
-	echo "OPTION:"
-	echo "  install       Install lightnode and dos client"
-	echo "  run           Run the client"
-	echo "  stop          Stop the client"
-	echo "  check         Check to see if client is running; Download log if client is not running"
-	echo "  proxyInfo     Print proxy information"
-	echo "  clientInfo    Print client information"
-	echo "  guardian      Trigger guardian"
-    exit 1
+    echo "OPTION:"
+    echo "  install       Install lightnode and dos client"
+    echo "  run           Run the client"
+    echo "  stop          Stop the client"
+    echo "  check         Check to see if client is running; Download log if client is not running"
+    echo "  proxyInfo     Print proxy information"
+    echo "  clientInfo    Print client information"
+    echo "  guardian      Trigger guardian"
     ;;
 esac
-
--- vps_docker.sh
+++ vps_docker.sh
@@ -1,149 +1,149 @@
-#/bin/bash
+#!/bin/bash
 
 setting="./dos.setting"
-# If file exists 
-if [[ -f "$setting" ]]
-then
-    source $setting
-	if [ -z "$DOSIMAGE" ]; 
-	then 
-		echo "Please assign a value to DOSIMAGE in dos.setting"
-		exit
-	fi
-	if [ -z "$USER" ]; 
-	then 
-		echo "Please assign a value to USER in dos.setting"
-		exit
-	fi
-	if [ -z "$VPSIP" ]; 
-	then 
-		echo "Please assign a value to VPSIP in dos.setting"
-		exit
-	fi
-	if [ -z "$VPSKEY" ]; 
-	then 
-		echo "Please assign a value to VPSKEY in dos.setting"
-		exit
-	fi
-	if [ -z "$KEYSTORE" ]; 
-	then 
-		echo "Please assign a value to KEYSTORE in dos.setting"
-		exit
-	fi
-	if [ -z "$GETHPOOL" ]; 
-	then 
-		echo "Please assign a value to GETHPOOL in dos.setting"
-		exit
-	fi
+# If file exists
+if [[ -f "$setting" ]]; then
+  source $setting
+  if [ -z "$DOSIMAGE" ]; then
+    echo "Please assign a value to DOSIMAGE field in dos.setting"
+    exit
+  fi
+  if [ -z "$USER" ]; then
+    echo "Please assign a value to USER field in dos.setting"
+    exit
+  fi
+  if [ -z "$IP" ]; then
+    echo "Please assign a value to IP field in dos.setting"
+    exit
+  fi
+  if [ -z "$SSHKEY" ]; then
+    echo "Please assign a value to SSHKEY field in dos.setting"
+    exit
+  fi
+  if [ -z "$KEYSTORE" ]; then
+    echo "Please assign a value to KEYSTORE in dos.setting"
+    exit
+  fi
+  if [ -z "$GETHPOOL" ]; then
+    echo "Please assign a value to GETHPOOL in dos.setting"
+    exit
+  fi
 else
-    echo "Can't find dos.setting"
-	exit
+  echo "Can't find dos.setting"
+  exit
 fi
-GETHPOOL=$GETHPOOL";ws://"$VPSIP":8546"
-DIR="/home/"$USER
+
+GETHPOOL="$GETHPOOL;ws://$IP:8546"
+if [ "$USER" == "root" ]; then
+  DIR="/root"
+else
+  DIR="/home/$USER"
+fi
 
 install_lightnode(){
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo apt-get update'
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo add-apt-repository -y ppa:ethereum/ethereum'
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo apt-get update'
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo apt-get install ethereum'
-  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $VPSKEY rinkeby.json $USER@$VPSIP:~/
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'geth --datadir='$DIR'/.rinkeby init rinkeby.json'
-  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $VPSKEY static-nodes.json $USER@$VPSIP:$DIR/.rinkeby/geth/static-nodes.json
-  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $VPSKEY geth.service  $USER@$VPSIP:~/
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo chmod 322 geth.service'
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo mv geth.service /lib/systemd/system/geth.service'
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo systemctl enable geth'
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo systemctl start geth'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo apt-get update'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo add-apt-repository -y ppa:ethereum/ethereum'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo apt-get update'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo apt-get install ethereum'
+  wget https://www.rinkeby.io/rinkeby.json -q -O rinkeby.json
+  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSHKEY rinkeby.json $USER@$IP:~/
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'geth --datadir='$DIR'/.rinkeby init rinkeby.json'
+  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSHKEY static-nodes.json $USER@$IP:$DIR/.rinkeby/geth/static-nodes.json
+  rm -f rinkeby.json
+  sed -i 's|xxx|'$DIR'|g' geth.service
+  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSHKEY geth.service  $USER@$IP:~/
+  git checkout -- geth.service
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo chmod 322 geth.service'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo mv geth.service /lib/systemd/system/geth.service'
+#  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo systemctl enable geth'
+#  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo systemctl start geth'
 }
 
 start_lightnode(){
   echo "start_lightnode"
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'ps cax | grep geth > /dev/null'
-  if [ $? -eq 0 ]
-  then
-	echo "Geth Process is running."
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'ps cax | grep geth > /dev/null'
+  if [ $? -eq 0 ]; then
+    echo "Geth Process is running."
   else
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo systemctl start geth'
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo systemctl start geth'
   fi
 }
 
 install_docker(){
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo apt-get update'
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'yes | sudo apt-get install docker.io'
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'sudo usermod -a -G docker $USER'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo apt-get update'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'yes | sudo apt-get install docker.io'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'sudo usermod -a -G docker $USER'
 }
 
 install_dos(){
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'mkdir -p '$DIR'/dos'
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'mkdir -p '$DIR'/credential'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'mkdir -p '$DIR'/dos'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'mkdir -p '$DIR'/credential'
   echo $KEYSTORE
-  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $VPSKEY $KEYSTORE $USER@$VPSIP:$DIR'/credential/'
+  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSHKEY $KEYSTORE $USER@$IP:$DIR'/credential/'
 }
 
 run(){
-    start_lightnode
-	result=$(ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP docker container ls | awk '(index($2, "dos") != 0) {print $2}')
-	newlog="doslog_$VPSIP"
-	if [ -z "$result" ]; 
-	then
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'sudo rm dos/doslog'
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'docker pull '$DOSIMAGE
-	echo -n Password:;read -s password ;
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'docker run -it -d \
--p 7946:7946 \
--p 8080:8080 \
--p 9501:9501 \
---mount type=bind,source='$DIR'/credential,target=/credential  \
---mount type=bind,source='$DIR'/dos,target=/dos  \
--e PUBLICIP="'$VPSIP'" \
--e GETHPOOL="'$GETHPOOL'" \
--e PASSPHRASE='$password'  \
--e CHAINNODE=rinkeby  \
--e APPSESSION="'$DOSVERSION'" \
--e APPNAME=DosClient  \
-'$DOSIMAGE
-	else echo "client is running";
-	fi
+  result=$(ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP docker container ls | awk '(index($2, "dos") != 0) {print $2}')
+  newlog="doslog_$IP"
+  if [ -z "$result" ]; then
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'sudo rm dos/doslog'
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'docker pull '$DOSIMAGE
+    echo -n Password:;read -s password ;
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'docker run -it -d \
+      -p 7946:7946 \
+      -p 8080:8080 \
+      -p 9501:9501 \
+      --mount type=bind,source='$DIR'/credential,target=/credential  \
+      --mount type=bind,source='$DIR'/dos,target=/dos  \
+      -e PUBLICIP="'$IP'" \
+      -e GETHPOOL="'$GETHPOOL'" \
+      -e PASSPHRASE='$password'  \
+      -e CHAINNODE=rinkeby  \
+      -e APPSESSION="'$DOSVERSION'" \
+      -e APPNAME=DosClient  \
+      '$DOSIMAGE
+  else
+    echo "client is running";
+  fi
 }
 
 stop(){
-	result=$(ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP docker container ls | awk '(index($2, "dos") != 0) {print $2}')
-	newlog="doslog_$VPSIP"
-	if [ -z "$result" ];
-	then echo "client is not running"
-	else
-	    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP  'docker stop $(docker ps -a -q)'
-		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP  'docker rm $(docker ps -a -q)'
-	fi
+  result=$(ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP docker container ls | awk '(index($2, "dos") != 0) {print $2}')
+  newlog="doslog_$IP"
+  if [ -z "$result" ]; then echo "client is not running"
+  else
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP  'docker stop $(docker ps -a -q)'
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP  'docker rm $(docker ps -a -q)'
+  fi
 }
 
 check(){
-	result=$(ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP docker container ls | awk '(index($2, "dos") != 0) {print $2}')
-	newlog="doslog_$VPSIP"
-	if [ -z "$result" ];
-	then scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $VPSKEY $USER@$VPSIP:~/dos/doslog $newlog;
-	else echo "client is running";
-	fi
+  result=$(ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP docker container ls | awk '(index($2, "dos") != 0) {print $2}')
+  newlog="doslog_$IP"
+  if [ -z "$result" ]; then
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSHKEY $USER@$IP:~/dos/doslog $newlog;
+  else
+    echo "client is running";
+  fi
 }
 
 proxyInfo(){
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'curl http://localhost:8080/proxy'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'curl http://localhost:8080/proxy'
 }
 
 clientInfo(){
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'curl http://localhost:8080/status'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'curl http://localhost:8080/status'
 }
 
 guardian(){
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $VPSKEY $USER@$VPSIP 'curl http://localhost:8080/guardian'
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt -i $SSHKEY $USER@$IP 'curl http://localhost:8080/guardian'
 }
 
 case "$1" in
   "install")
     install_lightnode
-	install_docker
-	install_dos
+    install_docker
+    install_dos
     ;;
   "run")
     run
@@ -165,14 +165,13 @@ case "$1" in
     ;;
   *)
     echo "Usage: bash vps_docker.sh [OPTION]"
-	echo "OPTION:"
-	echo "  install       Install Docker and setup directory for client"
-	echo "  run           Run the client from Docker Hub"
-	echo "  stop          Stop the client"
-	echo "  check         Check to see if client is running; Download log if client is not running"
-	echo "  proxyInfo     Print proxy information"
-	echo "  clientInfo    Print client information"
-	echo "  guardian      Trigger guardian"
-    exit 1
+    echo "OPTION:"
+    echo "  install       Install Docker and setup directory for client"
+    echo "  run           Run the client from Docker Hub"
+    echo "  stop          Stop the client"
+    echo "  check         Check to see if client is running; Download log if client is not running"
+    echo "  proxyInfo     Print proxy information"
+    echo "  clientInfo    Print client information"
+    echo "  guardian      Trigger guardian"
     ;;
 esac
