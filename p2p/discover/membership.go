51735b2094da73799a39d39fb336e6b104af75ae
ericnien <nien0223@gmail.com>
2019-05-29 00:09:57 -0700

    Rename file name
    Signed-off-by: ericnien <nien0223@gmail.com>

---
+++ p2p/discover/membership.go
@@ -0,0 +1,74 @@
+package discover
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+
+	"github.com/hashicorp/serf/serf"
+)
+
+type Membership interface {
+	Join(Ip []string) (num int, err error)
+	Leave()
+	Lookup(id []byte) (addr net.IP)
+	NumOfPeers() int
+	PeersIP() (addr []net.IP)
+}
+
+func NewSerfNet(Addr net.IP, id []byte) (Membership, error) {
+	var err error
+	serfNet := &SerfNet{}
+	conf := serf.DefaultConfig()
+	conf.Init()
+	conf.LogOutput = ioutil.Discard
+	conf.MemberlistConfig.LogOutput = ioutil.Discard
+	conf.MemberlistConfig.AdvertiseAddr = Addr.String()
+	conf.NodeName = string(id)
+	serfNet.serf, err = serf.Create(conf)
+
+	return serfNet, err
+}
+
+type SerfNet struct {
+	serf *serf.Serf
+}
+
+func (s *SerfNet) NumOfPeers() int {
+	return len(s.serf.Members())
+}
+
+func (s *SerfNet) Join(bootstrapIp []string) (num int, err error) {
+	num, err = s.serf.Join(bootstrapIp, true)
+	return
+}
+
+func (s *SerfNet) Leave() {
+	s.Leave()
+	return
+}
+
+func (s *SerfNet) Lookup(id []byte) (addr net.IP) {
+	members := s.serf.Members()
+	for i := 0; i < len(members); i++ {
+		if members[i].Name == string(id) {
+			return members[i].Addr
+		}
+	}
+	return
+}
+
+func (s *SerfNet) PeersIP() (addr []net.IP) {
+	fmt.Println("members len ", len(s.serf.Members()))
+
+	members := s.serf.Members()
+	for i := 0; i < len(members); i++ {
+
+		if members[i].Name != s.serf.LocalMember().Name && members[i].Status == serf.StatusAlive {
+			fmt.Println("localMember ", []byte(s.serf.LocalMember().Name), "members[i].Name ", []byte(members[i].Name), " status ", members[i].Status, " addr ", members[i].Addr)
+
+			addr = append(addr, members[i].Addr)
+		}
+	}
+	return
+}
